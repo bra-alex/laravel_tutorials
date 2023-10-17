28e59ad6a4e98ff081bb4dc074066ef46f65d61e
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-10-17 12:14:57 +0000

    add: relationships

--- laragigs/database/seeders/DatabaseSeeder.php
+++ laragigs/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -16,12 +17,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::factory()->create([
+            'name' => 'Alexander',
+            'email' => 'test@example.com',
+        ]);
 
-        Listing::factory(6)->create();
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
 
         // Listing::create(
         //     [
