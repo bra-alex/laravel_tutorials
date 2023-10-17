6ce6694007f259c1dd465821818991a787139180
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-10-17 11:42:08 +0000

    add: user login and register

--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Listing
 Route::get('/', [ListingController::class, 'index']);
 Route::post('/listings', [ListingController::class, 'store']);
 Route::get('/listings/create', [ListingController::class, 'create']);
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+
+// User
+Route::post('/users', [UserController::class, 'store']);
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/login', [UserController::class, 'getLogin']);
+Route::post('/logout', [UserController::class, 'logout']);
+Route::get('/register', [UserController::class, 'create']);
+
+
 
 // Routes with id
 /*
